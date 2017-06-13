d05bda4dcff6751b0f095d23636f9091d910f91d
System <none>
2017-06-13 21:14:31 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ var app = express();
 var pageSize = 2;
 
 // Load database
-var games = require('./.data/games.json');
+var gamesData = require('./.data/games.json');
 var gamesArray = [];
 //split games list into pages
-var gamesTemp = games.slice(0);
+var gamesTemp = gamesData.slice(0);
 while (gamesTemp.length > 0) {
     gamesArray.push(gamesTemp.splice(0, pageSize));
 }
+var games = {
+  count: gamesData.length,
+  paginated: gamesArray
+};
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
@@ -28,28 +32,27 @@ app.get("/games", function (request, response) {
   response.send(returnPaginated(games, request));
 });
 
+app.get("/games/:gameId", function(request, response) {
+  var payload = gamesData.slice(parseInt(request.params.gameId));
+  console.log(gamesData.slice(0));
+  //payload.url = currentURL(request);
+  response.send(payload);
+});
+
 function returnPaginated(data, request) {
   //set current page if specifed as get variable (eg: /?page=2)
   var currentPage = 1;
   if (typeof request.query.page !== 'undefined') {
-      currentPage = +request.query.page;
+      currentPage = parseInt(request.query.page);
   }
-  //show list of students from group
-  var gamesList = gamesArray[currentPage - 1];
   return {
-    count: games.length,
-    next: currentPage === gamesList.length ? null : currentURL(request) + '?page=' + (currentPage + 1),
+    count: data.count,
+    next: currentPage === data.paginated.length ? null : currentURL(request) + '?page=' + (currentPage + 1),
     previous: currentPage === 1 ? null : currentURL(request) + '?page=' + (currentPage - 1),
-    results: gamesList
+    results: data.paginated[currentPage - 1]
   };
 }
 
-app.get("/games/:gameId", function(request, response) {
-  var payload = games[request.params.gameId];
-  payload.url = currentURL(request);
-  response.send(games[request.params.gameId]);
-});
-
 function currentURL(request) {
-  return request.protocol + '://' + request.get('host') + request.route.path;
+  return request.protocol + '://' + request.get('host') + request.url.split('?')[0];
 }
