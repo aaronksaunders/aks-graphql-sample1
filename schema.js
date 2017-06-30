f2c0afa39976cd2a8854dd282ab2d3b775f75fa9
System <none>
2017-06-30 08:30:51 +0000

    Checkpoint

--- schema.js
+++ schema.js
@@ -69,7 +69,7 @@ const resolvers = {
       const filteredList = gamesData.filter(game =>
         game.title.includes(args.search || '')
       )
-      return Object.assign({}, { totalCount: gamesData.length }, connectionFromArray(filteredList, args))
+      return Object.assign({}, { totalCount: filteredList.length }, connectionFromArray(filteredList, args))
     }
   }
 }
